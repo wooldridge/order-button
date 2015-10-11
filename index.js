3e3208308cc6161605864e646240e136e77a48c8
Mike Wooldridge <mike@hellion.org>
2015-10-11 00:31:56 -0700

    play a random mac os x sound on button press

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var dash_button = require('node-dash-button');
+var dash_button = require('node-dash-button'),
+    sound = require("mac-sounds");
+
+var mac_sounds = [
+  "basso",
+  "blow",
+  "bottle",
+  "frog",
+  "funk",
+  "glass",
+  "hero",
+  "morse",
+  "ping",
+  "pop",
+  "purr",
+  "sosumi",
+  "submarine",
+  "tink"
+];
 
 var dash = dash_button("74:c2:46:ad:f5:b7"); //address from step above
 dash.on("detected", function (){
   var d = new Date();
   console.log("Button press detected: " + d);
+  var random_sound = mac_sounds[Math.floor(Math.random() * mac_sounds.length)];
+  sound(random_sound);
 });
